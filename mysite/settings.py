4428d094bf0b6e300b0e93bc9723591d9661aee8
dom13245493 <dan.v92@yandex.ru>
2025-09-13 20:26:09 +0000

    new ALLOWED_HOST 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3f58x!^*7!)4g_nt73(ai6t2^3r7pttbt)!)8tc&(6m#9d7+b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["dom13245493.pythonanywhere.com"]
 
 
 # Application definition
